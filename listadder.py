b0dfb1cf94a640ba381da566649ad6fe17033892
Sergiowk <34040046+Sergiowk@users.noreply.github.com>
2020-12-22 14:24:10 +0100

    Playlists function working (#1)
    * Playlist working
    
    Adding links to the playlists.txt (letting first link blank) is possible to add automatically more than one link.
    
    * Code cleaned
    
    Now the methods are on the functions file and listadder is the main (where all the functions are called).

--- listadder.py
+++ listadder.py
@@ -1,32 +1,10 @@
 from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.keys import Keys
+from functions import *
 
-#Driver initialization
-driver = webdriver.Chrome(".\chromedriver.exe")
-driver.get("https://iptvextreme.eu/") 
-#Letting the web loading
-sleep(5)
+driver = initialize()
 
-#Variables initialization
-mac = "MAC ADRESS"
-name = "Name"
-link = "www.iptvlist.com"
-
-#Getting the values from the web
-macAdress = driver.find_element_by_name('TxtMacAddress')
-playlistName = driver.find_element_by_name('TxtName')
-playlistLink = driver.find_element_by_name('TxtLink')
-saveList = driver.find_element_by_name('CmdSave')
-#elem.clear()
-
-#Introducing the values on the web
-macAdress.send_keys(mac)
-playlistName.send_keys(name)
-playlistLink.send_keys(link)
-#Saving
-saveList.click()
-#Letting the web loading
-sleep(5)
+insertlist(driver)
 
 
